3f508b54deb5564a2b9d02d07118b00bd7a57ad0
Zi Chen <zijchen@microsoft.com>
2022-05-10 16:07:01 -0700

    Target net472

--- Program.cs
+++ Program.cs
@@ -1,34 +1,44 @@
-﻿using Microsoft.SqlServer.Dac;
+﻿using System;
+using Microsoft.SqlServer.Dac;
 
-// Set the connection string
-string connectionString = "";
-DacServices service = new DacServices(connectionString);
-service.Message += (sender, e) => {
-    Console.WriteLine(e.Message);
-};
-
-// Publish test, set dacpac path and target database
-string publishSourceDacpac = "";
-string publishTargetDatabase = "";
-using (DacPackage dacpac = DacPackage.Load(publishSourceDacpac))
+namespace DacFx.Nuget.Test
 {
-    service.Publish(dacpac, publishTargetDatabase, new PublishOptions());
+    public class Program
+    {
+        public static void Main()
+        {
+            // Set the connection string
+            string connectionString = "";
+            DacServices service = new DacServices(connectionString);
+            service.Message += (sender, e) => {
+                Console.WriteLine(e.Message);
+            };
+
+            // Publish test, set dacpac path and target database
+            string publishSourceDacpac = "";
+            string publishTargetDatabase = "";
+            using (DacPackage dacpac = DacPackage.Load(publishSourceDacpac))
+            {
+                service.Publish(dacpac, publishTargetDatabase, new PublishOptions());
+            }
+
+            // Extract test, set dacpac path and source database
+            string extractSourceDatabase = "";
+            string extractTargetDacpac = "";
+            service.Extract(extractTargetDacpac, extractSourceDatabase, "testApp", new Version(1, 0));
+
+            // Import test, set bacpac path and target database
+            string importSourceBacpac = "";
+            string importTargetDatabase = "";
+            using (BacPackage bacpac = BacPackage.Load(importSourceBacpac))
+            {
+                service.ImportBacpac(bacpac, importTargetDatabase);
+            }
+
+            // Export test, set bacpac path and source database
+            string exportSourceDatabase = "";
+            string exportTargetBacpac = "";
+            service.ExportBacpac(exportTargetBacpac, exportSourceDatabase);
+        }
+    }
 }
-
-// Extract test, set dacpac path and source database
-string extractSourceDatabase = "";
-string extractTargetDacpac = "";
-service.Extract(extractTargetDacpac, extractSourceDatabase, "testApp", new Version(1, 0));
-
-// Import test, set bacpac path and target database
-string importSourceBacpac = "";
-string importTargetDatabase = "";
-using (BacPackage bacpac = BacPackage.Load(importSourceBacpac))
-{
-    service.ImportBacpac(bacpac, importTargetDatabase);
-}
-
-// Export test, set bacpac path and source database
-string exportSourceDatabase = "";
-string exportTargetBacpac = "";
-service.ExportBacpac(exportTargetBacpac, exportSourceDatabase);
